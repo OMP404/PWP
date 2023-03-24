5ea1f19bf57adb772f1aeb61341bbf90b4121494
Saku Antikainen <antikainensaku@gmail.com>
2023-03-24 13:08:44 +0200

    testing related mods

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 import json
+
+import click
 from flask import Flask, Response, request
+from flask.cli import with_appcontext
+from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Resource, Api
-from jsonschema import validate, ValidationError
+from jsonschema import ValidationError, validate
+from sqlalchemy import UniqueConstraint, event
 from sqlalchemy.engine import Engine
-from sqlalchemy import event, UniqueConstraint
 from sqlalchemy.exc import IntegrityError, OperationalError
-from werkzeug.exceptions import UnsupportedMediaType, NotFound, Conflict, BadRequest
+from werkzeug.exceptions import (BadRequest, Conflict, NotFound,
+                                 UnsupportedMediaType)
 from werkzeug.routing import BaseConverter
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///bar.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///Database/bar.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
 api = Api(app)
 db = SQLAlchemy(app)
+
 
 @event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
@@ -23,20 +28,23 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor.execute("PRAGMA foreign_keys=ON")
     cursor.close()
 
+
 class Bar(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True,nullable=False)
+    name = db.Column(db.String(64), unique=True, nullable=False)
     address = db.Column(db.String(64), nullable=True)
 
-    tapdrink = db.relationship("Tapdrink", back_populates="bar")
-    cocktail = db.relationship("Cocktail", back_populates="bar")
+    tapdrink = db.relationship(
+        "Tapdrink", cascade="all, delete-orphan", back_populates="bar")
+    cocktail = db.relationship(
+        "Cocktail", cascade="all, delete-orphan", back_populates="bar")
 
     def serialize(self):
         return {
             "name": self.name,
-            "address": self.address     
+            "address": self.address
         }
-    
+
     def deserialize(self, doc):
         self.name = doc["name"]
         self.address = doc["address"]
@@ -56,19 +64,22 @@ class Bar(db.Model):
             "description": "The address for the bar",
             "type": "string"
         }
-        
+
         return schema
+
 
 class Tapdrink(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    bar_name = db.Column(db.String(64), db.ForeignKey("bar.name",ondelete="CASCADE"))
+    bar_name = db.Column(db.String(64), db.ForeignKey(
+        "bar.name", ondelete="CASCADE"))
     drink_type = db.Column(db.String(64), unique=False, nullable=True)
     drink_name = db.Column(db.String(64), unique=False, nullable=False)
     drink_size = db.Column(db.Float, unique=False, nullable=False)
     price = db.Column(db.Float, unique=False, nullable=False)
-    table_args_= (UniqueConstraint('bar_name', 'drink_name', 'drink_size', name='No duplicates in a bar'),
-                     )
-    bar = db.relationship("Bar", cascade="all, delete-orphan", back_populates="tapdrink")
+    table_args_ = (UniqueConstraint('bar_name', 'drink_name', 'drink_size', name='No duplicates in a bar'),
+                   )
+    bar = db.relationship(
+        "Bar", back_populates="tapdrink")
 
     def serialize(self):
         return {
@@ -85,7 +96,7 @@ class Tapdrink(db.Model):
         self.drink_name = doc["drink_name"]
         self.drink_size = doc["drink_size"]
         self.price = doc["price"]
-    
+
     @staticmethod
     def json_schema():
         schema = {
@@ -117,32 +128,35 @@ class Tapdrink(db.Model):
         }
         return schema
 
+
 class Cocktail(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    bar_name = db.Column(db.String(64), db.ForeignKey("bar.name",ondelete="CASCADE"))
+    bar_name = db.Column(db.String(64), db.ForeignKey(
+        "bar.name", ondelete="CASCADE"))
     cocktail_name = db.Column(db.String(64), unique=False, nullable=False)
     price = db.Column(db.Float, nullable=False)
-    table_args_= (UniqueConstraint('bar_name', 'cocktail_name', name='No duplicates in a bar'),
-                     )
-    bar = db.relationship("Bar", cascade="all, delete-orphan", back_populates="cocktail")
+    table_args_ = (UniqueConstraint('bar_name', 'cocktail_name', name='No duplicates in a bar'),
+                   )
+    bar = db.relationship(
+        "Bar", back_populates="cocktail")
 
     def serialize(self):
         return {
             "bar_name ": self.bar_name,
             "cocktail_name ": self.cocktail_name,
-            "price": self.price    
+            "price": self.price
         }
-    
+
     def deserialize(self, doc):
         self.bar_name = doc["bar_name"]
         self.cocktail_name = doc["cocktail_name"]
-        self.address = doc["price"]
+        self.price = doc["price"]
 
     @staticmethod
     def json_schema():
         schema = {
             "type": "object",
-            "required": ["bar_name","cocktail_name", "price"]
+            "required": ["bar_name", "cocktail_name", "price"]
         }
         props = schema["properties"] = {}
         props["bar_name"] = {
@@ -158,8 +172,9 @@ class Cocktail(db.Model):
             "type": "number",
             "minimum": 0
         }
-        
+
         return schema
+
 
 class BarCollection(Resource):
 
@@ -168,7 +183,7 @@ class BarCollection(Resource):
             "bars": []
         }
         bars = Bar.query.all()
-        
+
         for bar in bars:
             body["bars"].append(
                 {
@@ -178,19 +193,18 @@ class BarCollection(Resource):
             )
 
         return Response(json.dumps(body), 200, mimetype=JSON)
-        
+
     def post(self):
         if not request.json:
             raise UnsupportedMediaType
-        
         try:
             validate(request.json, Tapdrink.json_schema())
         except ValidationError as e:
             raise BadRequest(description=str(e))
-        
+
         bar = Bar()
         bar.deserialize(request.json)
-                
+
         try:
             db.session.add(bar)
             db.session.commit()
@@ -202,6 +216,7 @@ class BarCollection(Resource):
 
         return Response(status=201, headers={'Location': api.url_for(BarItem, bar=bar)})
 
+
 class BarItem(Resource):
 
     def get(self, bar):
@@ -211,14 +226,14 @@ class BarItem(Resource):
     def put(self, bar):
         if not request.json:
             raise UnsupportedMediaType
-        
+
         try:
             validate(request.json, bar.json_schema())
         except ValidationError as e:
             raise BadRequest(description=str(e))
-        
+
         bar.deserialize(request.json)
-                
+
         try:
             db.session.add(bar)
             db.session.commit()
@@ -229,11 +244,12 @@ class BarItem(Resource):
             )
 
         return Response(status=204)
-        
+
     def delete(self, bar):
         db.session.delete(bar)
         db.session.commit()
         return Response(status=204)
+
 
 class TapdrinkCollection(Resource):
 
@@ -247,10 +263,10 @@ class TapdrinkCollection(Resource):
             "tapdrinks": []
         }
         tapdrinks = Tapdrink.query.filter_by(bar_name=bar).all()
-        
+
         for tapdrink in tapdrinks:
             body["tapdrinks"].append(
-                {   
+                {
                     "bar_name": tapdrink.bar_name,
                     "drink_type": tapdrink.drink_type,
                     "drink_name": tapdrink.drink_name,
@@ -260,19 +276,19 @@ class TapdrinkCollection(Resource):
             )
 
         return Response(json.dumps(body), 200, mimetype=JSON)
-        
+
     def post(self):
         if not request.json:
             raise UnsupportedMediaType
-        
+
         try:
             validate(request.json, Tapdrink.json_schema())
         except ValidationError as e:
             raise BadRequest(description=str(e))
-        
+
         tapdrink = Tapdrink()
         tapdrink.deserialize(request.json)
-                
+
         try:
             db.session.add(tapdrink)
             db.session.commit()
@@ -284,25 +300,28 @@ class TapdrinkCollection(Resource):
 
         return Response(status=201, headers={'Location': api.url_for(TapdrinkItem, tapdrink=tapdrink)})
 
+
 class TapdrinkItem(Resource):
 
     def get(self, bar, drinkname, drinksize):
-        tapdrink = Tapdrink.query.filter_by(bar_name=bar, drink_name=drinkname, drink_size=drinksize).first()
+        tapdrink = Tapdrink.query.filter_by(
+            bar_name=bar, drink_name=drinkname, drink_size=drinksize).first()
         body = tapdrink.serialize()
         return Response(json.dumps(body), 200, mimetype=JSON)
 
     def put(self, bar, drinkname, drinksize):
         if not request.json:
             raise UnsupportedMediaType
-        
+
         try:
             validate(request.json, Tapdrink.json_schema())
         except ValidationError as e:
             raise BadRequest(description=str(e))
-        
-        tapdrink = Tapdrink.query.filter_by(bar_name=bar, drink_name=drinkname, drink_size=drinksize).first()
+
+        tapdrink = Tapdrink.query.filter_by(
+            bar_name=bar, drink_name=drinkname, drink_size=drinksize).first()
         tapdrink.deserialize(request.json)
-                
+
         try:
             db.session.add(tapdrink)
             db.session.commit()
@@ -313,12 +332,14 @@ class TapdrinkItem(Resource):
             )
 
         return Response(status=204)
-        
+
     def delete(self, bar, drinkname, drinksize):
-        tapdrink = Tapdrink.query.filter_by(bar_name=bar, drink_name=drinkname, drink_size=drinksize).first()
+        tapdrink = Tapdrink.query.filter_by(
+            bar_name=bar, drink_name=drinkname, drink_size=drinksize).first()
         db.session.delete(tapdrink)
         db.session.commit()
         return Response(status=204)
+
 
 class CocktailCollection(Resource):
 
@@ -332,7 +353,7 @@ class CocktailCollection(Resource):
             "cocktails": []
         }
         cocktails = Cocktail.query.filter_by(bar_name=bar).all()
-        
+
         for cocktail in cocktails:
             body["cocktails"].append(
                 {
@@ -343,19 +364,19 @@ class CocktailCollection(Resource):
             )
 
         return Response(json.dumps(body), 200, mimetype=JSON)
-        
+
     def post(self):
         if not request.json:
             raise UnsupportedMediaType
-        
+
         try:
-            validate(request.json,Cocktail.json_schema())
+            validate(request.json, Cocktail.json_schema())
         except ValidationError as e:
             raise BadRequest(description=str(e))
-        
+
         cocktail = Cocktail()
         cocktail.deserialize(request.json)
-                
+
         try:
             db.session.add(cocktail)
             db.session.commit()
@@ -371,22 +392,24 @@ class CocktailCollection(Resource):
 class CocktailItem(Resource):
 
     def get(self, bar, cocktailname):
-        cocktail = Cocktail.query.filter_by(bar_name=bar, cocktail_name=cocktailname).first()
+        cocktail = Cocktail.query.filter_by(
+            bar_name=bar, cocktail_name=cocktailname).first()
         body = cocktail.serialize()
         return Response(json.dumps(body), 200, mimetype=JSON)
 
     def put(self, bar, cocktailname):
         if not request.json:
             raise UnsupportedMediaType
-        
+
         try:
             validate(request.json, cocktail.json_schema())
         except ValidationError as e:
             raise BadRequest(description=str(e))
-        
-        cocktail = Cocktail.query.filter_by(bar_name=bar, cocktail_name=cocktailname).first()
+
+        cocktail = Cocktail.query.filter_by(
+            bar_name=bar, cocktail_name=cocktailname).first()
         cocktail.deserialize(request.json)
-                
+
         try:
             db.session.add(cocktail)
             db.session.commit()
@@ -397,12 +420,14 @@ class CocktailItem(Resource):
             )
 
         return Response(status=204)
-    
+
     def delete(self, bar, cocktailname):
-        cocktail = Cocktail.query.filter_by(bar_name=bar, cocktail_name=cocktailname).first()
+        cocktail = Cocktail.query.filter_by(
+            bar_name=bar, cocktail_name=cocktailname).first()
         db.session.delete(cocktail)
         db.session.commit()
         return Response(status=204)
+
 
 class BarConverter(BaseConverter):
     def to_python(self, name):
@@ -410,15 +435,78 @@ class BarConverter(BaseConverter):
         if db_bar is None:
             raise NotFound
         return db_bar
-        
+
     def to_url(self, db_bar):
         return db_bar.name
+
+
+@ click.command("init-db")
+@ with_appcontext
+def init_db_command():
+    """
+    Initializes the database. If already initialized, nothing happens.
+    """
+    db.create_all()
+
+
+@ click.command("populate-db")
+@ with_appcontext
+def populate_db_command():
+    """
+    Populates the database with products
+    """
+    tap_drink_types = ["Beer", "Long Drink", "Cider"]
+    tap_drink_names = ['Karhu', "Koff", "CrowMoor"]
+    drink_prices = [4.5, 5.5, 6.5]
+    cocktail_names = ["Coctail", "Shot", "Drink"]
+    for i in range(1, 5):
+        bar = Bar(
+            name=f"Bar {i}",
+            address=f"Address Ave #{i}",
+        )
+        db.session.add(bar)
+    db.session.commit()
+    for i in range(3):
+        drink = Tapdrink(
+            bar_name=f"Bar {i + 1}",
+            drink_type=tap_drink_types[i],
+            drink_name=tap_drink_names[i],
+            drink_size=0.5,
+            price=drink_prices[i % 3]
+        )
+        db.session.add(drink)
+        coctail = Cocktail(
+            bar_name=f"Bar {i + 1}",
+            cocktail_name=cocktail_names[i],
+            price=drink_prices[i]
+        )
+        db.session.add(coctail)
+    db.session.commit()
+
+
+@ click.command("test-document")
+@ with_appcontext
+def test_document_command():
+    """
+    Prints the bars available in the database
+    """
+    with app.app_context():
+        products = Bar.query.all()
+        for product in products:
+            print(product.serialize())
+
 
 app.url_map.converters["bar"] = BarConverter
 
 api.add_resource(BarCollection, "/api/bars/")
 api.add_resource(BarItem, "/api/bars/<bar:bar>/")
 api.add_resource(TapdrinkCollection, "/api/bars/<bar:bar>/tapdrinks")
-api.add_resource(TapdrinkItem, "/api/bars/<bar:bar>/tapdrinks/<drinkname>/<drinksize>")
+api.add_resource(
+    TapdrinkItem, "/api/bars/<bar:bar>/tapdrinks/<drinkname>/<drinksize>")
 api.add_resource(CocktailCollection, "/api/bars/<bar:bar>/cocktails")
 api.add_resource(CocktailItem, "/api/bars/<bar:bar>/cocktails/<cocktailname>/")
+
+# Add cli commands to the app
+app.cli.add_command(init_db_command)
+app.cli.add_command(populate_db_command)
+app.cli.add_command(test_document_command)
