b292757e5bb979404eeb1a1b00bbeeb8337b23dd
JooC <55877330+sussasminister@users.noreply.github.com>
2023-05-07 01:51:59 +0300

    connection to Bars API

--- resc/auxillary.py
+++ resc/auxillary.py
@@ -1,4 +1,11 @@
+"""
+An Auxillary API for the Oulu Bars API, purpose of this work is to prove the workings of Oulu Bars API and to show one of its usecases.
+"""
+
+import json
+from operator import itemgetter
 import requests
+
 
 PORT = 5000
 BASE_URL = f"http://localhost:{PORT}"
@@ -12,3 +19,12 @@ def get_bars():
     bars = requests.get(f"{API_URL}bars/", timeout=TIMEOUT)
     if bars.status_code != 200:
         print("Unfortunately it seems to be that there is no bars to go :(")
+
+
+def show_bar_info(jsondata):
+    cocktails = jsondata['items']
+    print("this is info", cocktails)
+
+
+def show_error(response):
+    print("responded with an error: ", response.status_code)
